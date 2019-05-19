4054933650085431aef8c0e95e5f00eb3571573c
Rishav78 <rishavgarg789@gmail.com>
2019-05-20 03:43:51 +0530

    commit

--- Public/editInformation.js
+++ Public/editInformation.js
@@ -1,23 +1,54 @@
+var email = document.querySelector('.Email')
+var name = document.querySelector('.name')
+var dob = document.querySelector('.dob')
+var gender = document.querySelector('.gender')
+var phone = document.querySelector('.phoneno')
+var city = document.querySelector('.city')
+var interests = document.querySelector('.interests')
+var userdetail = document.querySelector('.userdetail')
+var expectation = document.querySelector('.expectations')
 $('#datepicker').datepicker({
     'format' : 'yyyy-mm-dd',
     'autoclose' : true
 });
-document.querySelector('form').onsubmit = (event)=>{
-	event.preventDefault()
-	var data = {
-		name : document.querySelector('.name').value,
-		dob : document.querySelector('.dob').value,
-		gender : document.querySelector('.gender').value,
-		phoneno : document.querySelector('.phoneno').value,
-		city : document.querySelector('.city').value,
-		interests : document.querySelector('.interests').value,
-		userdetail : document.querySelector('.userdetail').value,
-		expectations : document.querySelector('.expectations').value
-	}
+
+// document.querySelector('form').onsubmit = (event)=>{
+// 	event.preventDefault()
+// 	var data = {
+// 		name : document.querySelector('.name').value,
+// 		dob : dob.value,
+// 		gender : gender.value,
+// 		phoneno : phone.value,
+// 		city : city.value,
+// 		interests : interests.value,
+// 		userdetail : userdetail.value,
+// 		expectations : expectation.value
+// 	}
+// 	var req = new XMLHttpRequest()
+// 	req.onload = ()=>{
+// 		window.location = '/profile'
+// 	}
+// 	req.open('POST','/updateInfo')
+// 	req.send(JSON.stringify(data))
+// }
+
+window.onload = ()=>{
 	var req = new XMLHttpRequest()
 	req.onload = ()=>{
-		console.log('sdfgh')
+		var data = JSON.parse(req.responseText)
+		email.value = data.Email
+		document.querySelector('.name').value = data.Name
+		dob.value = data.DOB
+		gender.value = data.Gender
+		phone.value = data.Phno
+		city.value = data.City
+		userdetail.value = data.About
+		expectation.value = data.Expectations
 	}
-	req.open('POST','/updateInfo')
-	req.send(JSON.stringify(data))
+	req.open('GET','/getInformation')
+	req.send()
+}
+
+function upload(){
+	document.querySelector('input[type="file"]').click()
 }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var path = require('path')
+var multer = require('multer')
 var bodyParser = require('body-parser')
 var mysql = require('mysql')
 var session = require('express-session')
@@ -7,26 +8,55 @@ var MySQLStorage = require('express-mysql-session')(session)
 var passport = require('passport')
 var app = express()
 app.set('view engine','ejs')
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 app.use('/static',express.static(path.join(__dirname,'Public')))
-var con = mysql.createConnection({
+const con = mysql.createConnection({
 	host : 'localhost',
 	user : 'root',
 	password : '',
 	database : 'UCA_WebProject'
 })
-var sessionStore = new MySQLStorage({
+const sessionStore = new MySQLStorage({
 	host : 'localhost',
 	user : 'root',
 	password : '',
 	database : 'UCA_WebProject'
 })
+const storage = multer.diskStorage({
+	destination: './public/',
+	filename: function(req, file, cb){
+		cb(null,file.fieldname + path.extname(file.originalname))
+	}
+})
+const upload = multer({
+	storage: storage,
+	// limits : {
+	// 	fileSize: 10
+	// }
+	fileFilter: function(req,file,cb){
+		checkFileType(file,cb)
+	}
+}).single('myImage')
+function checkFileType(file,cb){
+	const filetypes = /jpeg|jpg|png|gif/
+	const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
+	const mimetype = filetypes.test(file.mimetype)
+	if(mimetype && extname){
+		return cb(null,true)
+	}else{
+		cb('error')
+	}
+}
 app.use(session({
 	secret : 'sdfghjkl',
 	resave : false,
 	saveUninitialized : false,
 	store : sessionStore
 }))
+Date
 app.use(passport.initialize())
 app.use(passport.session())
 con.connect((err)=>{
@@ -35,13 +65,13 @@ con.connect((err)=>{
 	// con.query('truncate table tags')
 	// con.query('drop table Users')
 	// con.query('truncate table Users')
-	// var q = `create table Users(Name char(100), Email varchar(100), Password varchar(100), Phno char(15), City char(100), Gender char(1), Date char(10), Role char(50), Status char(50), AcivationState char(5), LoginAs char(20))`
+	// var q = `create table Users(Name char(100), Email varchar(100), Password varchar(100), Phno char(15), City char(100), DOB char(10), Gender char(6), About char(250), Expectations char(250), Role char(50), Status char(50), ActivationState char(5), LoginAs char(20), Verified char(5))`
 	// var q = `create table Tags(Id int, name char(100), CreatedBy varchar(100), CreationDate date)`
 	// var q = 'create table CommunityList(CommunityName char(50), MembershipRule char(50), CommunityLocation varchar(50), CommunityOwner char(50), CreateDate date, CommunityPic varchar(200))'
 	// var q = `insert into communityList values('asdfg', 'sdfg', 'sdfgh' , 'werty', '0/0/0', 'dfghjk')`
-	// var q = `insert into Users values('Rishav', 'rishavgarg789@gmail.com', 'hacker12', '1234567890', 'Mandi GobindGarh', 'M', '13/9/1999', 'Superadmin', 'Confirmed', 'True', 'Admin')`
+	// var q = `insert into Users values('Rishav', 'rishavgarg789@gmail.com', 'hacker12', '1234567890', 'Mandi GobindGarh', '13/9/1999', 'Male', null, null, 'Superadmin', 'Confirmed', 'True', 'Admin', 'True')`
 	//  
-	// con.query()
+	// con.query(q)
 	console.log('connected...')
 })
 app.get('/',(req,res)=>{
@@ -49,14 +79,18 @@ app.get('/',(req,res)=>{
 		con.query(`select * from Users where Email = '${req.user}'`,(err,result)=>{
 			if(err) throw err
 				console.log(result[0].Role)
-			if(result[0].Role == 'User'){
-				return res.redirect('/profile')
-			}else{
-				if(result[0].LoginAs == 'Admin'){
-					return res.redirect('/admin/profile')
+			if(result[0].Verified == 'True'){
+				if(result[0].Role == 'User'){
+					return res.redirect('/profile')
 				}else{
-					return res.redirect('/communitypanel')
+					if(result[0].LoginAs == 'Admin'){
+						return res.redirect('/admin/profile')
+					}else{
+						return res.redirect('/communitypanel')
+					}
 				}
+			}else{
+				editInformation(req,res)
 			}
 		})
 	}
@@ -66,13 +100,17 @@ app.post('/login',(req,res)=>{
 	con.query(`select * from Users where Email = '${req.body.Email}' and Password = '${req.body.Password}'`,(err,result)=>{
 		if(err) throw err
 		if(result.length>0){
-			if(result[0].AcivationState == 'True'){
+			if(result[0].ActivationState == 'True'){
 				req.login(req.body.Email,(err)=>{
 					if(err) throw err
-					if(result[0].Role == 'User'){
-						return res.redirect('/profile')	
+					if(result[0].Verified == 'True'){
+						if(result[0].Role == 'User'){
+							return res.redirect('/profile')	
+						}else{
+							return res.redirect('/admin/profile')
+						}
 					}else{
-						return res.redirect('/admin/profile')
+						editInformation(req,res)
 					}
 				})
 			}else{
@@ -91,7 +129,7 @@ app.get('/admin/profile',isAuthenticated(),(req,res)=>{
 	con.query(`select * from Users where Email = '${req.user}'`,(err,result)=>{
 		if(err) throw err
 		var data = result[0]
-		res.render('Home',{data : data,visible : false})
+		return res.render('Home',{data : data,visible : false})
 	})
 })
 
@@ -104,16 +142,16 @@ app.get('/profile',isAuthenticated(),(req,res)=>{
 			return res.render('profile_user',{data : data, visible : true})
 		}else{
 			if(data.LoginAs == 'Admin'){
-				res.render('profile_admin',{data : data,visible : true})
+				return res.render('profile_superadmin_admin',{data : data,visible : true})
 			}else{
-				res.render('profile_user',{data : data,visible : true})
+				return res.render('profile_superadmin_user',{data : data,visible : true})
 			}
 		}
 	})
 })
 
 app.get('/admin/adduser',isAuthenticated(),(req,res)=>{
-	res.render('AddUser')
+	return res.render('AddUser')
 })
 
 app.post('/admin/adduser',(req,res)=>{
@@ -123,20 +161,19 @@ app.post('/admin/adduser',(req,res)=>{
 	})
 
 	req.on('end',()=>{
-		console.log(data)
 		data = JSON.parse(data)
 		con.query(`select * from Users where Email = '${data.email}'`,(err,result)=>{
 			if(err) throw err
 			console.log('dfghjkl')
-			if(result.length <= 0){
-				var q = `insert into Users values('${data.name}', '${data.email}', '${data.password}', '${data.phone}', '${data.city}', 'M', date '2015-12-17', '${data.role}', 'Pending', 'False', 'Admin')`
+			if(result.length == 0){
+				var q = `insert into Users(Name, Email, Password, Phno, City, Role, Status, ActivationState, LoginAs) values('${data.name}', '${data.email}', '${data.password}', '${data.phone}', '${data.city}', '${data.role}', 'Pending', 'False', 'Admin')`
 				con.query(q,(err,result)=>{
 					if(err) throw err;
 					console.log('add')
 					return res.send('User Added')
 				})
 			}else{
-				res.send('User Already Exist')
+				return res.send('User Already Exist')
 			}
 		})
 		
@@ -145,7 +182,7 @@ app.post('/admin/adduser',(req,res)=>{
 
 app.get('/admin/userlist',isAuthenticated(),(req,res)=>{
 	con.query('select * from Users',(err,result)=>{
-		res.render('ShowUser2',{data : result})
+		return res.render('ShowUser2',{data : result})
 	})
 })
 
@@ -160,22 +197,22 @@ app.post('/admin/userlist',(req,res)=>{
 		if(data.status == 'All' && data.userType == 'All'){
 			con.query(`select * from Users`,(err,result)=>{
 				if(err) throw err
-				res.send(JSON.stringify(getUser(data,result)))
+				return res.send(JSON.stringify(getUser(data,result)))
 			})
 		}else if(data.status != 'All' && data.userType == 'All'){
 			con.query(`select * from Users where Status = '${data.status}'`,(err,result)=>{
 				if(err) throw err
-				res.send(JSON.stringify(getUser(data,result)))
+				return res.send(JSON.stringify(getUser(data,result)))
 			})
 		}else if(data.status == 'All' && data.userType != 'All'){
 			con.query(`select * from Users where Role = '${data.userType}'`,(err,result)=>{
 				if(err) throw err
-				res.send(JSON.stringify(getUser(data,result)))
+				return res.send(JSON.stringify(getUser(data,result)))
 			})
 		}else{
 			con.query(`select * from Users where Status = '${data.status}' and Role = '${data.userType}'`,(err,result)=>{
 				if(err) throw err
-				res.send(JSON.stringify(getUser(data,result)))
+				return res.send(JSON.stringify(getUser(data,result)))
 			})
 		}
 	})
@@ -190,7 +227,7 @@ app.post('/update',(req,res)=>{
 		data = JSON.parse(data)
 		con.query(`update Users set Email = '${data.Email}', Phno = '${data.Phone}', City = '${data.City}', Status = '${data.Status}', Role = '${data.Role}' where Email = '${data.Email}'`,(err,result)=>{
 			if(err) throw err
-		  	res.send('done')
+		  	return res.send('done')
 		})
 	})
 })
@@ -217,9 +254,9 @@ app.post('/Activation',(req,res)=>{
 	req.on('end',()=>{
 		data = JSON.parse(data)
 		console.log(data)
-		con.query(`update Users set AcivationState = '${data.state}' where Email = '${data.user}'`,(err,result)=>{
+		con.query(`update Users set ActivationState = '${data.state}' where Email = '${data.user}'`,(err,result)=>{
 			if(err) throw err
-			res.send(data)
+			return res.send(data)
 		})
 	})
 })
@@ -230,12 +267,12 @@ app.get('/Logout',isAuthenticated(),(req,res)=>{
 	})
 	req.session.destroy((err)=>{
 		if(err) throw err
-		res.redirect('/')
+		return res.redirect('/')
 	})
 })
 
 app.get('/tags',(req,res)=>{
-	res.render('Tags')
+	return res.render('Tags')
 })
 
 app.post('/tags',(req,res)=>{
@@ -249,17 +286,17 @@ app.post('/tags',(req,res)=>{
 			if(result.length<=0){
 				con.query(`insert into tags values(${Math.random()*Math.pow(10,8)}, '${data}', '${req.user}', date '12/12/12')`,(err,result)=>{
 					if(err) throw err
-					res.send('added')
+					return res.send('added')
 				})
 			}else{
-				res.send('tag already exist')
+				return res.send('tag already exist')
 			}
 		})
 	})
 })
 
 app.get('/tagslist',(req,res)=>{
-	res.render('taglist2')
+	return res.render('taglist2')
 })
 
 app.post('/tagslistdata',(req,res)=>{
@@ -279,7 +316,7 @@ app.post('/tagslistdata',(req,res)=>{
 			// 	})
 			// 	res.send(array)
 			// }else{
-				res.send(result)
+				return res.send(result)
 			// }
 		})
 	})
@@ -293,7 +330,7 @@ app.post('/deletetag',(req,res)=>{
 	req.on('end',()=>{
 		con.query(`delete from tags where name = ${data}`,(err,result)=>{
 			if(err) throw err
-			res.send('done')
+			return res.send('done')
 		})
 	})
 })
@@ -305,9 +342,9 @@ app.get('/changePassword',(req,res)=>{
 			res.render('ChangePassword_user')
 		}else{
 			if(result[0].LoginAs == 'Admin'){
-				res.render('changePassword_superadmin_admin')
+				return res.render('changePassword_superadmin_admin')
 			}else{
-				res.render('ChangePassword_superadmin_user')
+				return res.render('ChangePassword_superadmin_user')
 			}
 		}
 	})
@@ -333,7 +370,7 @@ app.post('/changePassword',(req,res)=>{
 })
 
 app.get('/community/communityList',(req,res)=>{
-	res.render('communityList')
+	return res.render('communityList')
 })
 
 app.post('/community/communityList',(req,res)=>{
@@ -358,57 +395,55 @@ app.post('/community/communityList',(req,res)=>{
 })
 
 app.get('/manageCommunity',(req,res)=>{
-	res.render('manageCommunity')
+	return res.render('manageCommunity')
 })
 
 app.get('/communitypanel',(req,res)=>{
 	con.query(`update Users set LoginAs = 'User' where Email = '${req.user}'`,(err,result)=>{
 		if(err) throw err
 	})
-	con.query(`select LoginAs from Users where Email = '${req.user}'`,(err,result)=>{
+	con.query(`select * from Users where Email = '${req.user}'`,(err,result)=>{
 		if(err) throw err
-		if(result[0].LoginAs == 'User'){
-			res.render('communityManager_user')
+		if(result[0].Role == 'User'){
+			return res.render('communitypanel_user')
 		}else{
-			res.render('communityManager_admin')
+			return res.render('communitypanel_superadmin_user')
 		}
 	})
 })
 
 app.get('/notfound',(req,res)=>{
-	res.render('404NotFound')
+	return res.render('404NotFound')
 })
 
 app.get('/editInformation',(req,res)=>{
+	editInformation(req,res)
+})
+
+app.get('/getInformation',(req,res)=>{
 	con.query(`select * from Users where Email = '${req.user}'`,(err,result)=>{
 		if(err) throw err
-		var data = result[0]
-		if(data.LoginAs == 'Admin'){
-			res.render('editInformation_admin')
-		}else{
-			res.render('editInformation_user')
-		}
+		res.send(JSON.stringify(result[0]))
 	})
 })
 
 app.post('/updateInfo',(req,res)=>{
-	var data = ''
-	req.on('data',(chunk)=>{
-		data += chunk
-		console.log('dfgh')
-	})
-	req.on('end',()=>{
-		console.log(JSON.parse(data))
-		res.send(data)
-	})
+	console.log(req.body)
+	var data = req.body
+	// con.query(`update Users set Name = '${data.name}', DOB = '${data.dob}', Gender = '${data.gender}', Phno = '${data.phone}', City = '${data.city}', About = '${data.about}', Expectations = '${data.expectations}', Verified = 'True' where Email = '${req.user}'`,(err,result)=>{
+	// 	if (err) throw err
+	// 	res.redirect('/profile')
+	// })
+	res.redirect('/profile')
 })
 
-app.get('/normal',(req,res)=>{
-	con.query(`select * from Users where Email = '${req.user}'`,(err,result)=>{
-		if(err) throw err
-		var data = result[0]
-		res.render('profile_user',{data : data,visible : true})
-	})
+app.get('/faltu',(req,res)=>{
+	res.render('faltu')
+})
+
+app.post('/faltu',(req,res)=>{
+	console.log(req.body)
+
 })
 
 passport.serializeUser((user,done)=>{
@@ -424,7 +459,7 @@ function isAuthenticated(){
 		if(req.isAuthenticated()){
 			return next()
 		}
-		res.redirect('/')
+		return res.redirect('/')
 	}
 }
 
@@ -440,6 +475,23 @@ function getUser(data,users){
 	}else{
 		return users
 	} 
+}
+
+function editInformation(req,res) {
+	con.query(`select * from Users where Email = '${req.user}'`,(err,result)=>{
+		if(err) throw err
+		console.log(req.user)
+		var data = result[0]
+		if(data.Role == 'User'){
+			return res.render('editInformation_user')
+		}else{
+			if(data.LoginAs == 'Admin'){
+				return res.render('editInformation_superadmin_admin')
+			}else{
+				return res.render('editInformation_superadmin_user')
+			}
+		}
+	})
 }
 
 var Port = 8000
